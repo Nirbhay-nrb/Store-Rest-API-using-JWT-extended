c4ea92ae931da0a6434897fff9a86beb792c9499
Nirbhay <nmakhija_be20@thapar.edu>
2022-08-02 10:20:03 +0530

    adding Token Refresh resource and /refresh  end point

--- resources/user.py
+++ resources/user.py
@@ -1,6 +1,11 @@
 from flask_restful import Resource, reqparse
 from models.user import UserModel
-from flask_jwt_extended import create_access_token, create_refresh_token
+from flask_jwt_extended import (
+    create_access_token, 
+    create_refresh_token, 
+    jwt_required, 
+    get_jwt_identity
+)
 
 _user_parser = reqparse.RequestParser()
 _user_parser.add_argument(
@@ -69,3 +74,14 @@ class UserLogin(Resource):
         
         # user doesnt exist or password is wrong
         return {'message' : 'Invalid Credentials'}, 401 # means unauthorized
+
+
+# resource for token refreshing
+class TokenRefresh(Resource):
+    @jwt_required(refresh=True)
+    def post(self):
+        current_user = get_jwt_identity() 
+        # refresh token never changes, so using that we get the identity from the JWT and create a new access token which is not fresh
+        # to get a fresh access token, you need to provide the username and password again
+        new_token = create_access_token(identity=current_user, fresh=False) # this token is not fresh
+        return {'access_token' : new_token} , 200
